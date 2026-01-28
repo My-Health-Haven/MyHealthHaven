0a5277f480d7d9ea7178aa54904e601acd652360
Renusz <122424459+Renusz@users.noreply.github.com>
2026-01-28 10:40:30 -0600

    Update Schedule.jsx

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -18,11 +18,13 @@ const Schedule = () => {
             </Typography>
 
             <GlassCard sx={{ p: 0, overflow: 'hidden', height: '1200px' }}>
-                <iframe 
-                    src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ1ufROl2vbl4GPX-DDCCWxzK9dZR8bWymy5iDM2sL4T9nlJUkSwEmVr3BsGMb-ACdM2yF7mxji6?gv=true" 
-                    style={{ border: 0, width: '100%', height: '100%' }} 
-                    frameBorder="0"
-                ></iframe>
+                {/* Google Calendar Appointment Scheduling begin */}
+            <iframe 
+                src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ16B63n7brNHN9Y_vqxeKi7ymLdMa1b9atimdbiYwKA4RNY6mRKB1ilUFJq6E0X1VGcWuKloAdk?gv=true" 
+                style={{ border: 0, width: '100%', height: '100%' }} 
+                frameBorder="0"
+            ></iframe>
+            {/* end Google Calendar Appointment Scheduling */}
             </GlassCard>
         </Box>
       </Container>
